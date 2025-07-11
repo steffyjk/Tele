49c244c20b14c08e0ebe11babd55dfae48a0652f
steffyjk <steffy.jk2018@gmail.com>
2025-07-11 14:51:31 +0530

    [ Feat ] : added initial project set up

---
+++ telegram-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Header from '@/components/Header';
+
+export default function Home() {
+  return (
+    <div className="text-center mt-20 text-4xl font-bold text-white-500">
+      <Header/>
+    </div>
+  );
+}
---
+++ telegram-app/src/components/Header.tsx
@@ -0,0 +1,4 @@
+// src/components/Header.tsx
+export default function Header() {
+  return <h1 className="text-3xl font-bold text-center mt-10">Telegram QR Login</h1>
+}
